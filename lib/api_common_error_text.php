200b6914cf78b1d6c90d4ac53d157ac86fa03d7b
Mikayla Jones <mikaylaj12227@gmail.com>
2016-03-15 17:02:19 -0400

    Occurred has two R's Drew

--- lib/api_common_error_text.php
+++ lib/api_common_error_text.php
@@ -1,16 +1,16 @@
 <?php
 
 // error for when there is an issue with connecting to the MySQL database
-define('ERRTXT_DBCONN_FAILED', 'A connection error has occured. Contact the administrator.');
+define('ERRTXT_DBCONN_FAILED', 'A connection error has occurred. Contact the administrator.');
 
 // error for unauthorized user
 define('ERRTXT_UNATHORIZED', 'You are unauthorized to perform this action. Request permission from the administrator to continue.');
 
 // error for when a variable is not set that needs to be set
-define('ERRTXT_UNSETVARIABLE', 'A fatal error has occured on this page. Contact the administrator.');
+define('ERRTXT_UNSETVARIABLE', 'A fatal error has occurred on this page. Contact the administrator.');
 
 // error for when a query fails
-define('ERRTXT_FAILED_QUERY', 'Something whent wrong with your request. Go back and try again. If you keep getting this error, contact the administrator.');
+define('ERRTXT_FAILED_QUERY', 'Something went wrong with your request. Go back and try again. If you keep getting this error, contact the administrator.');
 
 // error for when the id of something does not exist
 define('ERRTXT_ID_NOT_FOUND', 'This resource does not exist. Please go back and try again. If you keep getting this error, contact the administrator.');
