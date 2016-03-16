22b96642608ac2fc3f924820ada47fb213cedacf
Charles E. Bevington IV <c.e.bevington@gmail.com>
2016-03-16 18:01:14 -0400

    All works but getting the input
    I got the writing to the DB working from variables and the errors are
    thrown at right time, only issue is I am unable to actually get the
    data from a post request

--- api/review_create_api.php
+++ api/review_create_api.php
@@ -4,30 +4,35 @@ Input (3 POST parameters): item_id, star_rating, description
 Process: Inserts inpuy into the ratings table
 Output: A boolean variable that is true on success (on failure returns the error)
 */
-require_once('../../lib/config.php');
-require_once('../../lib/http_headers.php');
-require_once('../../lib/api_common_error_text.php');
-require_once('../../lib/api_error_functions.php');
+require_once('../lib/config.php');
+require_once('../lib/http_headers.php');
+require_once('../lib/api_common_error_text.php');
+require_once('../lib/api_error_functions.php');
 
 // Set Up the Database Connection
 $databaseConnection = new mysqli(MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DBNAME);
 if ($databaseConnection->connect_errno != 0) {
     SendSingleError(HTTP_INTERNAL_ERROR, $databaseConnection->connect_error, ERRTXT_DBCONN_FAILED);
 }
+// Put data in variables
+$item_id = 5;//$_POST['item_id'];
+$star_rating = 4;//$_POST['star_rating'];
+$description = 'Loved It!';//$_POST['description'];
 
 // Check for Data
-if(!($_POST["item_id"] && $_POST["star_rating"])) {
-	SendSingleError(HTTP_INTERNAL_ERROR, 'one or more missing fields', ERRTXT_ID_NOT_FOUND);
+if(!($item_id && $star_rating)) {
+	SendSingleError(HTTP_INTERNAL_ERROR, $_POST['item_id'], ERRTXT_ID_NOT_FOUND);
 } else {
 	// Write data to database
-	$query = "INSERT INTO _table_
-		VALUES('0', $_POST['item_id'] && $_POST['star_rating'] && $_POST['description'], (SELECT GETDATE()))";
-	$result = mysqli_query($databaseConnection, $query, MYSQLI_STORE_RESULT);
-	if($result) // If query was successful
+
+	$query = "INSERT INTO ratings VALUES(0, $item_id, $star_rating, '". $description ."', CURRENT_DATE())";
+	if($databaseConnection->query($query)) { // If query was successful
 		header(HTTP_OK);
     	echo json_encode($result);
     	exit;
-	else // Otherwise send error
-		SendSingleError(HTTP_INTERNAL_ERROR, 'query failed', ERRTXT_FAILED);
+    }
 }
+
+SendSingleError(HTTP_INTERNAL_ERROR, /*'query failed'*/$query, ERRTXT_FAILED);
+
 ?>
