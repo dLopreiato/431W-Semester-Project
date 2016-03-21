a37e0cf791ec6b9fe59353e023e361564e0394d9
Andrew Lopreiato <drew.lopreiato@gmail.com>
2016-03-20 22:02:31 -0400

    Remove unnecessary files

--- api/examples/exception_test.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once('../../lib/config.php');
-require_once('../../lib/http_headers.php');
-require_once('../../lib/api_common_error_text.php');
-require_once('../../lib/api_error_functions.php');
-
-throw new Exception('Uncaught shit');
-
-?>
