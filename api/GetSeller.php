b665f3ba8542c434e880f2523efd1d4f2338c816
Mikayla Jones <mikaylaj12227@gmail.com>
2016-03-24 22:49:28 -0400

    added api to get information about a seller by username

---
+++ api/GetSeller.php
@@ -0,0 +1,39 @@
+<?php
+/*
+Input (1 GET parameter): username
+Process: Gets the user's seller info from the seller table
+Output: Returns an associative array of the user's seller info (sends single error on failure)
+*/
+require_once('../lib/config.php');
+require_once('../lib/http_headers.php');
+require_once('../lib/api_common_error_text.php');
+require_once('../lib/api_error_functions.php');
+
+// Set Up the Database Connection
+$databaseConnection = new mysqli(MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DBNAME);
+if ($databaseConnection->connect_errno != 0) {
+    SendSingleError(HTTP_INTERNAL_ERROR, $databaseConnection->connect_error, ERRTXT_DBCONN_FAILED);
+}
+// Put data in variables
+$username = (isset($_GET['username'])) ? ($_GET['username']) : (false);
+
+// Check for Data
+if(!($username)) {
+	SendSingleError(HTTP_BAD_REQUEST, "one or more fields not found", ERRTXT_UNSETVARIABLE);
+} else {
+	// get data from database
+	$query = "SELECT name, address, point_of_contact, phone FROM sellers WHERE username = '$username'";
+	$data = $databaseConnection->query($query);
+	if ($row = $data->fetch_assoc()){
+    	header(HTTP_OK);
+		header(API_RESPONSE_CONTENT);
+    	echo json_encode($row);
+	    exit;
+	} else  {
+	   SendSingleError(HTTP_BAD_REQUEST, 'user not found', ERRTXT_FAILED_QUERY);
+	}
+}
+
+SendSingleError(HTTP_INTERNAL_ERROR, 'php failed', ERRTXT_FAILED);
+
+?>
