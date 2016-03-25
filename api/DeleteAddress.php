3faadc7874ffd00f3bd2dfc85203ab20acc32ef7
Mikayla Jones <mikaylaj12227@gmail.com>
2016-03-24 23:34:01 -0400

    added api to delete an address belonging to the current active user by address_id

--- api/DeleteAddress.php
+++ api/DeleteAddress.php
@@ -1,7 +1,7 @@
 <?php
 /*
-Input: card_number to delete
-Process: Deletes this credit card from the active user's credit card list
+Input: address_id to delete
+Process: Deletes this address from the active user's address list
 Output: True on success, error otherwise
 */
 require_once('../lib/config.php');
@@ -20,20 +20,20 @@ if ($databaseConnection->connect_errno != 0) {
 // Put data in variables
 $username = (isset($_SESSION['username'])) ? ($_SESSION['username']) : (false);
 
-$card_number = (isset($_GET['card_number'])) ? ($_GET['card_number']) : (false);
+$address_id = (isset($_GET['address_id'])) ? ($_GET['address_id']) : (false);
 
 // Check for Data
 if(!($username)) {
 	SendSingleError(HTTP_UNAUTHORIZED, 'no user is logged in', ERRTXT_UNAUTHORIZED);
-} else if (!($card_number)){
+} else if (!($address_id)){
 	SendSingleError(HTTP_BAD_REQUEST, "one or more fields not found", ERRTXT_UNSETVARIABLE);
 } else {
 	
-	$query = "SELECT * FROM credit_cards WHERE card_number = '$card_number' AND username = '$username'";
+	$query = "SELECT * FROM addresses WHERE address_id = '$address_id' AND username = '$username'";
 	$data = $databaseConnection->query($query);
 
 	if ($data->num_rows > 0) {
-		$query = "DELETE FROM credit_cards WHERE card_number = '$card_number' AND username = '$username' ";	
+		$query = "DELETE FROM addresses WHERE address_id = '$address_id' AND username = '$username' ";	
 		if($databaseConnection->query($query)) { // If query was successful
 				header(HTTP_OK);
 				header(API_RESPONSE_CONTENT);
@@ -41,11 +41,11 @@ if(!($username)) {
 				exit;
 			}
 		else{
-			SendSingleError(HTTP_INTERNAL_ERROR, 'failed to delete card from database', ERRTXT_FAILED_QUERY);
+			SendSingleError(HTTP_INTERNAL_ERROR, 'failed to delete address from database', ERRTXT_FAILED_QUERY);
 		}
 	}
 	else{
-		SendSingleError(HTTP_BAD_REQUEST, 'credit card with this number does not exist or it does not belong to this user', ERRTXT_FAILED_QUERY);
+		SendSingleError(HTTP_BAD_REQUEST, 'address with this ID does not exist or it does not belong to this user', ERRTXT_FAILED_QUERY);
 	}
 }
 
