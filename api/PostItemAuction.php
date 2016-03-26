8722d09e8d6ae4d4488643461abd6a57fb431cc6
Charles E. Bevington IV <c.e.bevington@gmail.com>
2016-03-26 17:09:34 -0400

    Creating PostItemAuction.php
    Also removed a hard coded username,  I then opened a github issue
    because the session method returns all caps.

--- api/PostItemAuction.php
+++ api/PostItemAuction.php
@@ -19,23 +19,23 @@ if ($databaseConnection->connect_errno != 0) {
 // Put data in variables
 $item_id = (isset($_POST['item_id'])) ? ($_POST['item_id']) : (false);
 $username = $_SESSION['username'];
-$listed_price = (isset($_POST['listed_price'])) ? ($_POST['listed_price']) : (false);
+$reserve_price = (isset($_POST['reserve_price'])) ? ($_POST['reserve_price']) : (false);
 $number_in_stock = (isset($_POST['number_in_stock'])) ? ($_POST['number_in_stock']) : (false);
 
 // Check for Data
-if(!($item_id && $listed_price && $number_in_stock)) {
+if(!($item_id && $reserve_price && $number_in_stock)) {
 	SendSingleError(HTTP_BAD_REQUEST, "one or more fields not found", ERRTXT_ID_NOT_FOUND);
 } else {
 	// Check ownership
 	// Write data to database
-	$query = "INSERT INTO sold_by (item_id, username, listed_price, number_in_stock) VALUES($item_id, '". $username . "', '" . $listed_price . "', $number_in_stock)";
+	$query = "INSERT INTO auctioned_by (item_id, username, reserve_price, number_in_stock) VALUES($item_id, '". $username . "', '" . $reserve_price . "', $number_in_stock)";
 	if($databaseConnection->query($query)) { // If query was successful
 		header(HTTP_OK);
 		header(API_RESPONSE_CONTENT);
     	echo json_encode(TRUE);
     	exit;
     } else {
-        SendSingleError(HTTP_INTERNAL_ERROR, 'failed to post item for sale: ' . $query, ERRTXT_FAILED_QUERY);
+        SendSingleError(HTTP_INTERNAL_ERROR, 'failed to post item for auction: ' . $query, ERRTXT_FAILED_QUERY);
     }
 }
 
