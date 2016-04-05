ed835941b43dc3b2734f57708f10852bccdb8860
Mikayla Jones <mikaylaj12227@gmail.com>
2016-04-05 13:48:23 -0400

    added api to get all transactions belonging to the user that's logged in

---
+++ api/GetTransactions.php
@@ -0,0 +1,56 @@
+<?php
+/*
+Input: none
+Process:  Gets all transactions for this user (bids that turned to sales, purchases, rentals)
+Output: An array of all the transactions belonging to this user
+*/
+require_once('../lib/config.php');
+require_once('../lib/http_headers.php');
+require_once('../lib/api_common_error_text.php');
+require_once('../lib/api_error_functions.php');
+
+session_start();
+
+// Set Up the Database Connection
+$databaseConnection = new mysqli(MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DBNAME);
+if ($databaseConnection->connect_errno != 0) {
+    SendSingleError(HTTP_INTERNAL_ERROR, $databaseConnection->connect_error, ERRTXT_DBCONN_FAILED);
+}
+// Put data in variables
+$username = (isset($_SESSION['username'])) ? ($_SESSION['username']) : (false);
+
+if(!($username)) {
+	SendSingleError(HTTP_UNAUTHORIZED, 'no user is logged in', ERRTXT_UNAUTHORIZED);
+} else {
+	
+	$salesResult = array();
+	$rentalResult = array();
+	
+	// get data from database
+	$query = "SELECT * FROM sales WHERE username = '$username' ";
+	$data = $databaseConnection->query($query);
+	if ($data->num_rows > 0) {
+		while ($row =$data->fetch_assoc()){
+			$salesResult[] = $row;
+		}
+	}
+	
+	$query = "SELECT * FROM rental_transaction WHERE rented_out_to_username = '$username' ";
+	$data = $databaseConnection->query($query);
+	if ($data->num_rows > 0) {
+		while ($row =$data->fetch_assoc()){
+			$rentalResult[] = $row;
+		}
+	}
+	
+	$result = array("purchases" => $salesResult, "rentals" => $rentalResult);
+	
+	header(HTTP_OK);
+	header(API_RESPONSE_CONTENT);
+	echo json_encode($result);
+	exit;
+}
+
+SendSingleError(HTTP_INTERNAL_ERROR, 'php failed', ERRTXT_FAILED);
+
+?>
