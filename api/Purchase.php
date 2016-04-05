909be650b62ddc6bdf40153866560fc32b104e89
Lauren Bosse <lbosse4@gmail.com>
2016-04-04 22:31:41 -0400

    Added purchase product api

---
+++ api/Purchase.php
@@ -0,0 +1,47 @@
+<?php
+/*
+Input (1 POST parameters): username
+Process: Inserts input into the sales table
+Output: A boolean variable that is true on success (on failure returns the error)
+*/
+require_once('../lib/config.php');
+require_once('../lib/http_headers.php');
+require_once('../lib/api_common_error_text.php');
+require_once('../lib/api_error_functions.php');
+
+session_start();
+
+// Set Up the Database Connection
+$databaseConnection = new mysqli(MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DBNAME);
+if ($databaseConnection->connect_errno != 0) {
+    SendSingleError(HTTP_INTERNAL_ERROR, $databaseConnection->connect_error, ERRTXT_DBCONN_FAILED);
+}
+
+// Put data in variables
+$username = (isset($_SESSION['username'])) ? ($_SESSION['username']) : (false);
+
+$amount = (isset($_GET['amount'])) ? ($_GET['amount']) : (false);
+$item_id = (isset($_GET['item_id'])) ? ($_GET['item_id']) : (false);
+$card_number = (isset($_GET['card_number'])) ? ($_GET['card_number']) : (false);
+$sent = (isset($_GET['sent'])) ? ($_GET['sent']) : (false);
+$received = (isset($_GET['received'])) ? ($_GET['received']) : (false);
+
+// Check for Data
+if(!($username)) {
+	SendSingleError(HTTP_BAD_REQUEST, "one or more fields not found", ERRTXT_ID_NOT_FOUND);
+} else {
+	// Write data to database
+	$query = "INSERT INTO sales (amount, time, username, item_id, card_number, sent, received) VALUES(amount, CURRENT_TIME(), username, item_id, card_number, sent, received)";
+	if($databaseConnection->query( $query)) { // If query was successful
+		header(HTTP_OK);
+		header(API_RESPONSE_CONTENT);
+    	echo json_encode(TRUE);
+    	exit;
+    } else {
+        SendSingleError(HTTP_INTERNAL_ERROR, 'failed to complete purchase transaction.', ERRTXT_FAILED_QUERY);
+    }
+}
+
+SendSingleError(HTTP_INTERNAL_ERROR, 'php failed', ERRTXT_FAILED);
+
+?>
