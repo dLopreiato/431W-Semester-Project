d11e34af10bf3f2d0f42b5022a1be83c3ef1b066
Andrew Lopreiato <drew.lopreiato@gmail.com>
2016-04-08 15:25:00 -0400

    Finish blank template

---
+++ js/navbar-ajax.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    setActiveUserInfo();
+});
+
+function setActiveUserInfo() {
+    $.ajax({
+        url: PROTOCOL + ROOT_DIRECTORY + '/api/GetActiveUserInfo.php',
+        dataType: 'json',
+        success: function(data) {
+            if (data != false) {
+                $('#navbar-links').append('<li><a href="logout.html">Logout</a></li>');
+                $('#navbar-links').append('<li><a href="#">' + data['name'] + '</a></li>');
+            }
+            else {
+                $('#navbar-links').append('<li><a href="login.html">Login</a></li>');
+                $('#navbar-links').append('<li><a href="register.html">Register</a></li>');
+            }
+        },
+        error: function(xhr, ajaxOptions, thrownError) {
+            var serverErrorInfo = JSON.parse(unescape(xhr.responseText));
+            for (var key in serverErrorInfo) {
+                console.error('AJAX Error: ' + serverErrorInfo[key]['errorDescription'] + "\n" + thrownError);
+            }
+        }
+    });
+}
+
+function submitSearch() {
+    var searchTerm = $('input:text[name=searchBar]').val();
+
+    console.log(searchTerm); // delete this when implementing. this is just to show you what it is
+}
