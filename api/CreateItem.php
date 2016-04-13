8cdd1f922ea8558154dcad575a6c9b21e7bcf2db
Charles E. Bevington IV <c.e.bevington@gmail.com>
2016-04-12 20:17:49 -0400

    Adding CreateItem
    need to work on images and incrementing id

---
+++ api/CreateItem.php
@@ -0,0 +1,37 @@
+<?php
+require_once('../lib/config.php');
+require_once('../lib/http_headers.php');
+require_once('../lib/api_common_error_text.php');
+require_once('../lib/api_error_functions.php');
+
+// Set Up the Database Connection
+$databaseConnection = new mysqli(MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DBNAME);
+if ($databaseConnection->connect_errno != 0) {
+    SendSingleError(HTTP_INTERNAL_ERROR, $databaseConnection->connect_error, ERRTXT_DBCONN_FAILED);
+}
+// Put data in variables
+$item_id = 0;
+$description = (isset($_POST['description'])) ? ($_POST['description']) : (false);
+$location = (isset($_POST['location'])) ? ($_POST['location']) : (false);
+$category_id = (isset($_POST['category_id'])) ? ($_POST['category_id']) : (false);
+$image = (isset($_POST['image'])) ? ($_POST['image']) : (false);
+
+// Check for Data
+if(!($item_id && $star_rating)) {
+	SendSingleError(HTTP_BAD_REQUEST, "one or more fields not found", ERRTXT_ID_NOT_FOUND);
+} else {
+	// Write data to database
+	$query = "INSERT INTO items (item_id, description, location, image) VALUES($item_id, '". $description ."', '". $location ."', '". $image ."'";
+	if($databaseConnection->query($query)) { // If query was successful
+		header(HTTP_OK);
+		header(API_RESPONSE_CONTENT);
+    	echo json_encode(TRUE);
+    	exit;
+    } else {
+        SendSingleError(HTTP_INTERNAL_ERROR, 'failed to insert new item into database', ERRTXT_FAILED_QUERY);
+    }
+}
+
+SendSingleError(HTTP_INTERNAL_ERROR, 'php failed', ERRTXT_FAILED);
+
+?>
