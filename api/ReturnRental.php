dd28208f48bf6e4381144f7b291f31a9e404f732
Mikayla Jones <mikaylaj12227@gmail.com>
2016-04-14 00:30:22 -0400

    refactored returnrental to work based on rental_id

--- api/ReturnRental.php
+++ api/ReturnRental.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Input (2 GET parameters): item_id, serial_number
+Input (1 GET parameters): rental_id
 Process: Username taken from active user, sets on_shelf to 1, adds balance due if item is late
 Output: True on success, error on failure
 */
@@ -20,25 +20,25 @@ if ($databaseConnection->connect_errno != 0) {
 // Put data in variables
 $username = (isset($_SESSION['username'])) ? ($_SESSION['username']) : (false);
 
-$item_id = (isset($_GET['item_id'])) ? ($_GET['item_id']) : (false);
-$serial_number = (isset($_GET['serial_number'])) ? ($_GET['serial_number']) : (false);
+$rental_id = (isset($_GET['rental_id'])) ? ($_GET['rental_id']) : (false);
 
 // Check for Data
 if(!($username)) {
 	SendSingleError(HTTP_UNAUTHORIZED, 'no user is logged in', ERRTXT_UNAUTHORIZED);
-} else if (!($item_id &&  $serial_number)){
+} else if (!($rental_id)){
 	SendSingleError(HTTP_BAD_REQUEST, "one or more fields not found", ERRTXT_UNSETVARIABLE);
 } else {
-	$query = "SELECT * FROM rentables WHERE item_id = '$item_id' AND serial_number = '$serial_number' AND seller_username = '$username'";	
+	
+	$query = "SELECT * FROM rentables R, rental_transaction T WHERE T.rental_id = '$rental_id' AND R.seller_username = '$username' AND T.was_returned = 0 AND T.item_id = R.item_id AND T.serial_number = R.serial_number";	
 	$data = $databaseConnection->query($query);
-	$query2 = "SELECT * FROM rental_transaction WHERE item_id = '$item_id' AND serial_number = '$serial_number' AND was_returned = 0";	
-	$data2 = $databaseConnection->query($query2);
-    if ($data->num_rows > 0 && $data2->num_rows > 0) {
+    if ($data->num_rows > 0) {
+		
+		$row = $data->fetch_assoc();
+		$item_id = $row["item_id"];
+		$serial_number = $row["serial_number"];
 
 		// If it's late, add a fee
-		$row = $data->fetch_assoc();
-		$row2 = $data2->fetch_assoc();
-		if (getdate() > $row2["due_date"]){
+		if (date("Y-m-d H:i:s") > $row["due_date"]){
 			$balance_due = $row["rental_price"];
 			$query = "UPDATE sellers SET balance_due = balance_due + '$balance_due' WHERE username = '$username' ";
 			if(!($databaseConnection->query($query))) { 
@@ -47,7 +47,7 @@ if(!($username)) {
 		}
 		
 		$query = "UPDATE rentables SET on_shelf = 1 WHERE item_id = '$item_id' AND serial_number = '$serial_number' ";	
-		$query2 = "UPDATE rental_transaction SET was_returned = 1 WHERE item_id = '$item_id' AND serial_number = '$serial_number' AND was_returned = 0 ";
+		$query2 = "UPDATE rental_transaction SET was_returned = 1 WHERE rental_id = '$rental_id' AND was_returned = 0 ";
 		if($databaseConnection->query($query) && $databaseConnection->query($query2)) { // If query was successful
 					header(HTTP_OK);
 					header(API_RESPONSE_CONTENT);
@@ -57,7 +57,7 @@ if(!($username)) {
 			SendSingleError(HTTP_INTERNAL_ERROR, 'failed to update rental status in database', ERRTXT_FAILED_QUERY);
 		}
 	}else{
-		SendSingleError(HTTP_UNAUTHORIZED, 'user is not the seller of this item or item does not exist', ERRTXT_UNAUTHORIZED);
+		SendSingleError(HTTP_UNAUTHORIZED, 'user is not the seller of this item or item does not exist/is currently not rented out', ERRTXT_UNAUTHORIZED);
 	}
 }
 
