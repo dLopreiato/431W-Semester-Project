79b226bea4d59db04629bf8089c6c9ce1cd96d86
Mikayla Jones <mikaylaj12227@gmail.com>
2016-04-20 16:15:11 -0400

    fixed bugs in CreateItem.php,

--- api/CreateItem.php
+++ api/CreateItem.php
@@ -10,18 +10,17 @@ if ($databaseConnection->connect_errno != 0) {
     SendSingleError(HTTP_INTERNAL_ERROR, $databaseConnection->connect_error, ERRTXT_DBCONN_FAILED);
 }
 // Put data in variables
-$item_id = 0;
 $description = (isset($_POST['description'])) ? ($_POST['description']) : (false);
 $location = (isset($_POST['location'])) ? ($_POST['location']) : (false);
 $category_id = (isset($_POST['category_id'])) ? ($_POST['category_id']) : (false);
 $image = (isset($_POST['image'])) ? ($_POST['image']) : (false);
 
 // Check for Data
-if(!($item_id && $star_rating)) {
+if(!($description && $location && $category_id && $image)) {
 	SendSingleError(HTTP_BAD_REQUEST, "one or more fields not found", ERRTXT_ID_NOT_FOUND);
 } else {
 	// Write data to database
-	$query = "INSERT INTO items (item_id, description, location, image) VALUES($item_id, '". $description ."', '". $location ."', '". $image ."'";
+	$query = "INSERT INTO items (description, location, category_id, image) VALUES( '". $description ."', '". $location ."', '" .$category_id . "', '". $image ."')";
 	if($databaseConnection->query($query)) { // If query was successful
 		header(HTTP_OK);
 		header(API_RESPONSE_CONTENT);
