5e7660ec26aad93b87be8114e521f95d42ba639c
Andrew Lopreiato <drew.lopreiato@gmail.com>
2016-04-22 17:33:22 -0400

    Finish product functionality

--- api/EnterBid.php
+++ api/EnterBid.php
@@ -14,22 +14,22 @@ if ($databaseConnection->connect_errno != 0) {
 $databaseConnection->set_charset(MYSQL_CHARSET);
 // Put data in variables  item_id, amount, address_id, card_number
 $username = (isset($_SESSION['username'])) ? ($_SESSION['username']) : (false);
-$bid_id = 0;
-$item_id = (isset($_POST['item_id'])) ? ($_POST['item_id']) : (false);
-$amount = (isset($_POST['amount'])) ? ($_POST['amount']) : (false);
-$bid_won = "FALSE";
+$item_id = (isset($_GET['item_id'])) ? ($_GET['item_id']) : (false);
+$amount = (isset($_GET['amount'])) ? ($_GET['amount']) : (false);
+$card_number = (isset($_GET['card_number'])) ? ($_GET['card_number']) : (false);
+$address_id = (isset($_GET['address_id'])) ? ($_GET['address_id']) : (false);
 
 // Check for User Login
-if (!(isset($_SESSION['login']) && $_SESSION['login'] != '')) {
+if (!(isset($_SESSION['username']))) {
 	SendSingleError(HTTP_INTERNAL_ERROR, 'no user logged in', ERRTXT_UNAUTHORIZED);
 }
 
 // Check for Data
-if($item_id === false || $star_rating === false) {
+if($item_id === false || $amount === false || $card_number === false || $address_id === false) {
 	SendSingleError(HTTP_BAD_REQUEST, "one or more fields not found", ERRTXT_ID_NOT_FOUND);
 } else {
 	// Write data to database
-	$query = "INSERT INTO bids (amount, bid_id, time, username, item_id, bid_won) VALUES('". $amount ."', $bid_id, NOW() , '". $username ."', item_id, '". $bid_won ."'";
+	$query = "INSERT INTO bids (amount, time, username, item_id, card_number, address_id) VALUES('$amount', NOW() , '$username', $item_id, $card_number, $address_id)";
 	if($databaseConnection->query($query)) { // If query was successful
 		header(HTTP_OK);
 		header(API_RESPONSE_CONTENT);
