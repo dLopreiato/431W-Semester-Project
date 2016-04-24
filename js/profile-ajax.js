62241258a1832af9dd375bb3bbc509c3324f1ee2
Mikayla Jones <mikaylaj12227@gmail.com>
2016-04-23 21:19:51 -0400

    profile now pulls actual badge data

--- js/profile-ajax.js
+++ js/profile-ajax.js
@@ -1,12 +1,21 @@
 $(document).ready(function() {
-    var sendData = {username:$.urlParam('username');};
+	displayHeading();
+	populateBadges();
+});
+
+function displayHeading(){
+	var appendString = '<h1 class="media-heading">' + $.urlParam('username') + '\'s Badges</h1>';
+	$('#name-heading').append(appendString);
+}
+
+function populateBadges(){
+    var sendData = {username:$.urlParam('username')};
 
     $.ajax({
         url: PROTOCOL + ROOT_DIRECTORY + '/api/GetUserBadges.php',
         dataType: 'json',
         data: sendData,
         method: 'GET',
-
         success: function(data) {
             // data is array of username, badge_id, units_earned, last updated
             if (!data) {
@@ -14,7 +23,7 @@ $(document).ready(function() {
             }
             else {
                 $.each(data, function (i, item) {
-                    addBAdgeToTable(item.badge_id, item.units_earned, item.last_updated);
+                   addBadgeToTable(item.badge_id, item.units_earned, item.total_units, item.last_updated, item.title, item.icon, item.description);
                 });
             }
         },
@@ -27,14 +36,36 @@ $(document).ready(function() {
         }
     });
 }
-});
+
 
 $.urlParam = function(name){
     var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
     return results[1] || 0;
 }
 
-function addBadgeToTable(badgeID, unitsEarned, lastUpdated) {
-    var appendString = '<tr id="' + badgeID + '"><td>' + unitsEarned + '</td><td>' + lastUpdated + '</td></tr>';
-    $('#earned-badges tbody').append(appendString);
+function addBadgeToTable(badgeID, unitsEarned, totalUnits, lastUpdated, badgeTitle, badgeImage, badgeDescription) {
+	
+	var percentage = (unitsEarned/totalUnits) * 100;
+	
+    var appendString = '<div class="media"><div class="media-left media-middle"><a href="#"><img class="media-object" src="' + badgeImage + '" alt="' + badgeTitle + '" width="200" height="200"></a></div><div class="media-body">';
+	
+	appendString += '<h3 class="media-heading">' + badgeTitle + '</h3><br><p>' + badgeDescription + '</p><p>' + unitsEarned + '/' + totalUnits + ' Purchaces Complete </p><p>Last Updated: ' + lastUpdated;
+	appendString += '</p><div class="progress"><div class="';
+	
+	if (percentage < 100){
+		appendString += 'progress-bar progress-bar-info';
+	}
+	else{
+		appendString += 'progress-bar progress-bar-success';
+	}
+	
+	appendString +='" role="progressbar" aria-valuenow="' + percentage + '" aria-valuemin="0" aria-valuemax="100" style="width:' + percentage + '%">';
+	appendString += '<span class="sr-only">' + percentage + '% Complete</span></div></div></div></div>';
+	
+    $('#earned-badges').append(appendString);
+}
+
+function displayGeneralUserError(textToDisplay) {
+	var divText = '<div class="alert alert-danger alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>' + textToDisplay + '</div>';
+    $('#error-view').append(divText);
 }
