10bdc2800feef29a2f4c8e15499b8068bf106ec9
Mikayla Jones <mikaylaj12227@gmail.com>
2016-04-26 21:46:08 -0400

    added API to get current highest bid on an item

---
+++ api/GetHighestCurrentBid.php
@@ -0,0 +1,39 @@
+<?php
+/*
+Input (1 GET parameter): item_id
+Process: Gets the max current bid on an item
+Output: Returns the highest bid
+*/
+require_once('../lib/config.php');
+require_once('../lib/http_headers.php');
+require_once('../lib/api_common_error_text.php');
+require_once('../lib/api_error_functions.php');
+
+// Set Up the Database Connection
+$databaseConnection = new mysqli(MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DBNAME);
+if ($databaseConnection->connect_errno != 0) {
+    SendSingleError(HTTP_INTERNAL_ERROR, $databaseConnection->connect_error, ERRTXT_DBCONN_FAILED);
+}
+$databaseConnection->set_charset(MYSQL_CHARSET);
+// Put data in variables
+$item_id = (isset($_GET['item_id'])) ? ($_GET['item_id']) : (false);
+
+// Check for Data
+if($item_id === false) {
+	SendSingleError(HTTP_INTERNAL_ERROR, "one or more fields not found", ERRTXT_ID_NOT_FOUND);
+} else {
+		$result = array();
+        $query = "SELECT B.* FROM bids B WHERE B.item_id = '$item_id' AND B.amount >= ALL (SELECT B2.amount FROM bids B2 WHERE B2.item_id = B.item_id) ORDER BY B.item_id";
+		$data = $databaseConnection->query($query);
+	
+		$row =$data->fetch_assoc();
+
+	header(HTTP_OK);
+	header(API_RESPONSE_CONTENT);
+	echo json_encode($row);
+    exit;
+}
+
+SendSingleError(HTTP_INTERNAL_ERROR, 'php failed', ERRTXT_FAILED);
+
+?>
